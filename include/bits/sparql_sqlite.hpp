a580e8a69ad50e1186fae20f945dfc5e5f59e564
Sunny S <3030712@gmail.com>
2014-10-05 20:03:25 -0700

    Created a model_store for sqlite.
    In order to create an sqlite statment, the char * query has to be stored
    in the model_store intill it can be run. It can also be discared this way
    in case the sparql statement has an error.

--- include/bits/sparql_sqlite.hpp
+++ include/bits/sparql_sqlite.hpp
@@ -5,34 +5,77 @@
 #ifndef DATA_PATTERN_BITS_SPARQL_SQLITE_HPP
 #define DATA_PATTERN_BITS_SPARQL_SQLITE_HPP
 
+#include <string>
+
 namespace data_pattern {
 namespace bits {
-/**/
+/* sqlite_store */
+template <>
+model_store<sqlite>{
+public:
+  inline static bool
+  enabled();
+
+  std::string query;
+};
+
+/* sparql_sqlite_traits */
 template <>
 sparql_traits<sqlite>{
 public:
   inline static void
-  select(sqlite &);
+  select(sqlite &, model_store<sqlite> &);
 
   inline static void
-  where(sqlite &);
+  where(sqlite &, model_store<sqlite> &);
 
   inline static void
-  ask(sqlite &);
+  ask(sqlite &, model_store<sqlite> &);
 
   inline static void
-  describe(sqlite &);
+  describe(sqlite &, model_store<sqlite> &);
 
   inline static void
-  create(sqlite &);
+  create(sqlite &, model_store<sqlite> &);
 };
 
 template <>
 void
 sparql_traits<sqlite>::select(
   sqlite & _db
+, model_store<sqlite> & _store
 ){
-_db.prepare
+_store.query.push_back("SELECT ");
+}
+
+template <>
+sparql_traits<sqlite>::where(
+  sqlite & _db
+, model_store<sqlite> & _store
+){
+_store.query.push_back("WHERE ");
+}
+
+template <>
+sparql_traits<sqlite>::ask(
+  sqlite & _db
+, model_store<sqlite> & _store
+){
+}
+
+template <>
+sparql_traits<sqlite>::describe(
+  sqlite & _db
+, model_store<sqlite> & _store
+){
+}
+
+template <>
+sparql_traits<sqlite>::create(
+  Model & _db
+, model_store<sqlite> & _store
+){
+
 }
 
 } /* bits */ } /* data_pattern */
