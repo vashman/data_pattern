dd7643c120930667c9b5394ac92d7bb144a811fd
Sunny S <3030712@gmail.com>
2014-11-16 04:13:35 -0800

    removed remnant of sqlite access function.

--- include/sqlite.hpp
+++ include/sqlite.hpp
@@ -130,46 +130,6 @@ private:
   signed int state;
 };
 
-		col_type = type_begin;
-			for (size_t col=0; col_type != type_end; ++col, ++col_type){
-			switch(*col_type){
-			case 't':
-				auto buff = use_typebuffer<string>(*this);
-				buff_text.push((char*)sqlite3_column_text(statement, col));
-				break;
-			case 'i':
-				sqlite3_column_int(statement, col) >> this;
-				break;
-			case 'd':
-				sqlite3_column_double(statement, col) >> this;
-				break;
-			case 'b':
-				(void*)sqlite3_column_blob(statement, col) >> this;
-				break;
-			default:
-			}
-			}
-		rc = sqlite3_step(this->statement);
-		}
-		sqlite3_finalize(statement);
-			if (col_type != type_end){
-			return database::fail_bit; // Types left unused after step.
-			}
-		// Go through possible products of statements
-		switch(rc){
-		case SQLITE_OK:
-		case SQLITE_DONE:
-			return dataerr::PASS;
-		case SQLITE_BUSY:
-			return dataerr::FAIL;
-		default:
-			return dataerr::FAIL;
-		}
-	} else {
-	return dataerr::FAIL;
-	}
-}
-
 } /* data_pattern */
 #include "bits/sqlite.tcc"
 #endif
--- src/sqlite.cpp
+++ src/sqlite.cpp
@@ -1,6 +1,9 @@
 //
 
 //          Copyright Sundeep S. Sangha 2013 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef DATA_PATTERN_SQLITE_CPP
 #define DATA_PATTERN_SQLITE_CPP
@@ -16,31 +19,105 @@ sqlite3_close(this->db);
 delete zErrMsg;
 }
 
-int
-sqlite::prepare(
-  char const * _zsql
-, int _nbyte
-, sqlite_3_stmt ** _ppstmt
-, char const ** _pztail
+void
+sqlite::step(
+  sqlite_statement const & _stmt
 ){
-return sqlite3_prepare_v2(this->db, _zsql, _nbyte, _ppstmt, __pztail);
+_stmt.state = sqlite3_step(_stmt.stmt);
+  if (_stmt.state == SQLITE_OK){
+  _stmt.max_col = sqlite3_column_count();
+  }
 }
 
-int
-sqlite::bind_int(
-  sqlite::stmt_type * _stmt
-, int _index
+void
+sqlite::finalize(
+  sqlite_statement const & _stmt
 ){
-typesystems::typebuffer_interface<int> & buff = typesystems::use_typebuffer<int>(this->typesys);
-sqlite3_bind_int(_stmt, index, buff.next());
-buff.pop();
+sqlite3_finilize(_stmt.stmt);
 }
 
-int
-sqlite::step( 
+/* sqlite create */
+sqlite_statement
+sqlite::create(
+  char const * _query
+, int _size
+, char const **
 ){
-sqlite3_step(this->statement);
+sqlite_statement stmt();
+stmt.state = sqlite_3_prepare_v2(this->db, _query, _size, &stmt->stmt, 0);
+return stmt;
 }
+
+/* sqlite_statement */
+/* sqlite_statement bind_int */
+void
+sqlite_statement::bind(
+  int _index
+, int _var
+){
+this->state = sqlite3_bind_int(&this->stmt, _index, _var);
+}
+
+/* sqlite_statement bind_blob */
+void
+sqlite_statement::bind(
+  int _index
+, void const * _blob
+, int _size
+){
+this->state = sqlite3_bind_blob(&this->stmt, _index, _blob, _size
+              , SQLITE_TRANSIENT);
+}
+
+/* sqlite_statement bind */
+void
+sqlite_statement::bind(
+  int _index
+, char const * _str
+){
+this->state = sqlite3_bind_text(&this->stmt, _index, _str
+              , std::char_traits<char>::length(_str), SQLITE_TRANSIENT);
+}
+
+/* sqlite_statement bind */
+void
+sqlite_statement::bind(
+  int _index
+, double _var
+){
+this->state = sqlite3_bind_double(&this->stmt, _index, _var);
+}
+
+void
+sqlite_statement::bind(
+  int _index
+){
+this->state = sqlite3_bind_null(&this->stmt, _index);
+}
+
+/* sqlite_statement column_int */
+int
+sqlite_statement::column_int(
+  int _index
+){
+  if (_index > this->max_col){
+  throw ;
+  }
+return sqlite3_column_int(&this->stmt, _index);
+}
+
+/* sqlite_statement column_double */
+double
+sqlite_statement::column_double(
+  int _index
+){
+  if (_index > this->max_col){
+  throw ;
+  }
+return sqlite3_column_double(&this->stmt, _index);
+}
+
+/* sqlite rewriters */
 
 } /* data_pattern */
 #endif
