4ad85f7e7a1a8f85a8d736f8d790bcdb0ce5cd73
Sunny S <3030712@gmail.com>
2014-11-16 18:13:40 -0800

    added int rewriter in sqlite_rewriters

---
+++ include/bits/sqlite_rewriters.hpp
@@ -0,0 +1,49 @@
+// rewriters for pod sqlite types
+
+//          Copyright Sundeep S. Sangha 2013 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef DATA_PATTERN_BITS_SQLITE_HPP
+#define DATA_PATTERN_BITS_SQLITE_HPP
+
+#include <typesystems/get_rewriter.hpp>
+#include <typesystems/put_rewriter.hpp>
+
+namespace data_pattern {
+namespace bits {
+namespace sqlite {
+
+class int_put_rw : public typesystems::put_rewriter<int>{
+public:
+  int_put_rewriter(
+    std::size_t _refs = 0
+  );
+
+private:
+  virtual bool
+  do_rewrite(
+    int const &
+  , typesystems::typebuffer_container const &
+  ) const;
+
+  static typesystems::explicit_typeid_type const array[];
+};
+
+int_put_rw::int_put_rewriter(
+  std::size_t _refs
+)
+  : typesystems::put_rewriter<int>(
+      array
+    , static_cast<std::size_t>(1)
+    , _refs){
+}
+
+typesystems::explicit_typeid_type const int_put_rw::array[]
+  = {
+     typesysetms::explicit_typeid<sqlite_statement>::raw_typeid();
+  };
+
+} /* sqlite */ } /* bits */ } /* data_patten */
+#endif
