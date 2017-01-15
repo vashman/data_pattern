6071989d210598e701544cd5879ea979aa02f073
Sunny Sangha <3030712@gmail.com>
2017-01-15 00:49:53 -0800

    updated copyright notices

--- include/stream_model.hpp
+++ include/stream_model.hpp
@@ -1,6 +1,6 @@
 //
 
-//          Copyright Sundeep S. Sangha 2013 - 2015.
+//          Copyright Sundeep S. Sangha 2013 - 2017.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
